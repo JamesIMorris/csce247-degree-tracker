722af5e5d76a8dfb2584f637a42167be19c730aa
JDCJDC103 <jdc015643@gmail.com>
2024-02-22 12:51:40 -0500

    mostly finished stub for issue #38

---
+++ Course.java
@@ -0,0 +1,31 @@
+public class Course {
+  private String courseID;
+  private String courseName;
+  private String courseDescription;
+  private int creditHours;
+  private ArrayList<Seasons> semesterAvailability;
+  private ArrayList<String> preRequisites;
+  private ArrayList<String> coRequisites;
+  private CourseType type;
+
+  public Course(String courseName, String courseID, String courseDescription, int creditHours, ArrayList<Season> semester Availability, ArrayList<String> preRequisites, ArrayList<String> coRequisites, boolean isOverlay){
+
+  }
+
+  public boolean isAvailable(Season season){
+
+  }
+
+  public boolean isPlacement(){
+
+  }
+
+  public String getSubject(){
+
+  }
+
+  public int getCourseNumber(){
+    
+  }
+
+}
