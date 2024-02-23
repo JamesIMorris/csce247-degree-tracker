b466cd912386dfada2c13da9be052511cd5fb549
genesisraag <genraag@gmail.com>
2024-02-23 07:19:05 -0500

    fixed #6

---
+++ Major.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class Major {
+    private String name;
+    private String school;
+    private String department;
+    private ArrayList<Requirement> requirements;
+
+    public boolean addRequirement(String name, Category category, ArrayList<String> courseIDs, int creditsRequired) {
+        return false;
+    }
+
+    public boolean addRequirement(Requirement requirement) {
+        return false;
+    }
+
+    public Requirement getRequirement(String name) {
+
+    }
+}
