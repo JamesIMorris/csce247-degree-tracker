51c53c49770a9d318f08bffaa48474c332be284b
JDCJDC103 <jdc015643@gmail.com>
2024-02-23 02:53:47 -0500

    worked on issue #20

---
+++ CourseList.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+public class CourseList {
+  private CourseList courseList;
+  private ArrayList<Course> courses;
+
+  private CourseList(){
+
+  }
+
+  public CourseList getInstance(){
+
+  }
+
+  public boolean addCourse(String courseName, String courseID, String courseDescription, Int creditHours, ArrayList<Season> semesterAvailability, ArrayList<String> preRequisites, ArrayList<String> coRequisites, CourseType type){
+
+  }
+
+  public Course getCourse(String name){
+
+  }
+
+  public Course getCourse(String id){
+
+  }
+}
