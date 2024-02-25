9798fb365d3c5c8a8a24e2907d03d04c480722ac
genesisraag <genraag@gmail.com>
2024-02-24 20:43:47 -0500

    finished issue #8

---
+++ Category.java
@@ -0,0 +1,17 @@
+public enum Category {
+    CC,
+    CC-AIU,
+    CC-ARP,
+    CC-CMS,
+    CC-CMW,
+    CC-GFL,
+    CC-GHS,
+    CC-GSS,
+    CC-INF,
+    CC-INT,
+    CC-SCI,
+    CC-VSR,
+    CR,
+    MR,
+    PR
+}
