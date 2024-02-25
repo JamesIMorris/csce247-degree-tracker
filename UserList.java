6fd2c98cea89be8a7a09f9f00017d474491b719d
Isaac Morris <xxjamesblond007xx@gmail.com>
2024-02-25 17:36:46 -0500

    Fixed createNewUser #60

--- UserList.java
+++ UserList.java
@@ -19,7 +19,7 @@ public class UserList{
         return users;
     }
 
-    public boolean createNewUser(String username, String password, String firstName, String lastName, String email, String userType){
+    public boolean createNewUser(String username, String password, String firstName, String lastName, String email, UserType userType){
         return false;
     }
     
