b2b5883d9f5b6ae81bbf4fa3332d47e7f18e2bb2
genesisraag <genraag@gmail.com>
2024-02-24 21:32:38 -0500

    finished #9

---
+++ Status.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+public enum Status {
+    COMPLETE,
+    FINAL_STEP,
+    STARTED,
+    NOT_STARTED
+}
+
+public Status getStatus(ArrayList<StudentCourse> courses) {
+
+}
+
+public Status getStatus(Course course) {
+
+}
