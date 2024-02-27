9ee9dcf72216da58a44dd297cce564b9c7583509
Isaac Morris <xxjamesblond007xx@gmail.com>
2024-02-27 11:59:43 -0500

    Finished #36

--- Credit.java
+++ Credit.java
@@ -1,12 +1,37 @@
 import java.util.ArrayList;
 
 public class Credit {
+    public class PossibleRequirement{
+        Requirement requirement;
+        boolean possible;
+
+        public PossibleRequirement(Requirement requirement){
+            this.requirement = requirement;
+        }
+        public PossibleRequirement(Requirement requirement, boolean possible){
+            this.possible = possible;
+        }
+
+        public Requirement getRequirement(){
+            return requirement;
+        }
+        public void setRequirement(Requirement requirement){
+            this.requirement = requirement;
+        }
+        public boolean getPossible(){
+            return possible;
+        }
+        public void setPossible(boolean possible){
+            this.possible = possible;
+        }
+    }
+
     private Course course;
     private Semester semesterTaken;
     private int grade;
     private CreditType type;
     private int requirementsAssignedTo;
-    private ArrayList<Requirement> possibleRequirements;
+    private PossibleRequirement possibleRequirements;
     private String note;
 
     public Credit(Course course) {
@@ -16,6 +41,7 @@ public class Credit {
     public Credit(String courseID) {
 
     }
+
 
     public Credit(Course course, Semester semesterTaken, int grade, CreditType type, int requirementsAssignedTo,
             String note) {
@@ -31,6 +57,43 @@ public class Credit {
     public Credit(String courseID, Semester semesterTaken, int grade, CreditType type, int requirementsAssignedTo,
             String note) {
 
+    }
+
+    public Course getCourse(){
+        return course;
+    }
+    public void setCourse(Course course){
+        this.course = course;
+    }
+    public Season getSemesterTaken(){
+        return semesterTaken;
+    }
+    public void setSemesterTaken(Season semeseter){
+        semeseterTaken = semester;
+    }
+    public int getGrade(){
+        return grade;
+    }
+    public void setGrade(int grade){
+        this.grade = grade;
+    }
+    public CreditType getType(){
+        return type;
+    }
+    public void setType(CreditType type){
+
+    }
+    public int getRequirementsAssignedTo(){
+        return requirementsAssignedTo;
+    }
+    public String getNote(){
+        return note;
+    }
+    public void setNote(String note){
+        this.note = note;
+    }
+    public PossibleRequirement getPossibleRequirements(){
+        return possibleRequirements;
     }
 
     public String getCourseName() {
