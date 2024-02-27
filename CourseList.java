8d9e86d9e7475f38c7b8bc31b28be1ca05ea8fc1
genesisraag <genraag@gmail.com>
2024-02-27 18:38:41 -0500

    finished #78 and working on #79

--- CourseList.java
+++ CourseList.java
@@ -1,25 +1,28 @@
 import java.util.ArrayList;
+
 public class CourseList {
   private CourseList courseList;
   private ArrayList<Course> courses;
 
-  private CourseList(){
+  private CourseList() {
 
   }
 
-  public CourseList getInstance(){
+  public CourseList getInstance() {
 
   }
 
-  public boolean addCourse(String courseName, String courseID, String courseDescription, Int creditHours, ArrayList<Season> semesterAvailability, ArrayList<String> preRequisites, ArrayList<String> coRequisites, CourseType type){
+  public boolean addCourse(String courseName, String courseID, String courseDescription, Int creditHours,
+      ArrayList<Season> semesterAvailability, ArrayList<String> preRequisites, ArrayList<String> coRequisites,
+      CourseType type) {
 
   }
 
-  public Course getCourse(String name){
+  public Course getCourse(String name) {
 
   }
 
-  public Course getCourse(String id){
+  public Course getCourse(String id) {
 
   }
 }
--- DegreeTracker.java
+++ DegreeTracker.java
@@ -1,32 +1,34 @@
-public class DegreeTracker{
+public class DegreeTracker {
     private static DegreeTracker degreeTracker;
     private String error;
     private UserList userList;
     private MajorList majorList;
     private CourseList courseList;
 
-    private DegreeTracker(){
+    private DegreeTracker() {
         error = "";
         userList = UserList.getInstance();
         majorList = MajorList.getInstance();
         courseList = CourseList.getInstance();
     }
 
-    public DegreeTracker getInstance(){
-        if(degreeTracker == null)
+    public static DegreeTracker getInstance() {
+        if (degreeTracker == null)
             degreeTracker = new DegreeTracker();
         return degreeTracker;
     }
 
-    public void addError(String errorMessage){
+    public void addError(String errorMessage) {
         error += "\n" + errorMessage;
     }
 
-    public boolean login(String username, String password){
+    public boolean login(String username, String password) {
         return userList.login(username, password);
     }
 
-    public boolean signup(String username, String password, String firstName, String lastName, String email, UserType type){
+    public boolean signup(String username, String password, String firstName, String lastName, String email,
+            UserType type) {
         return userList.signup(username, password, firstName, lastName, email, type);
     }
+
 }
