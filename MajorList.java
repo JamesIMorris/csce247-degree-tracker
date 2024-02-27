7a034baa17a871df0b0c6fc821740d95d43ad3df
genesisraag <genraag@gmail.com>
2024-02-27 18:47:40 -0500

    merge main into majorlist-stub

--- MajorList.java
+++ MajorList.java
@@ -5,19 +5,23 @@ public class MajorList {
     private ArrayList<Major> majors;
 
     private MajorList() {
-
+        majors = new ArrayList<>();
     }
 
     public static MajorList getInstance() {
-
+        if (majorList == null) {
+            majorList = new MajorList();
+        }
+        return majorList;
     }
 
     public boolean addMajor(String name, String school, String department, ArrayList<Requirement> requirements) {
-
+        Major major = new Major(name, school, department, requirements);
+        return addMajor(major);
     }
 
     public boolean addMajor(Major major) {
-        return false;
+        return majors.add(major);
     }
 
     public ArrayList<Major> addMajors(ArrayList<Major> majors) {
