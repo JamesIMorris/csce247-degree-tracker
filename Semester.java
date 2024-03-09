003604418e3df5c4787e3b95ee418066c3305780
Isaac Morris <xxjamesblond007xx@gmail.com>
2024-03-09 15:35:06 -0500

    Finished #52

--- Semester.java
+++ Semester.java
@@ -4,6 +4,23 @@ public class Semester{
   private Season season;
 
   public Semester(int year, Season season){
+    this.year = year;
+    this.season = season;
+  }
 
+  public int getYear() {
+    return year;
+  }
+
+  public void setYear(int year) {
+    this.year = year;
+  }
+
+  public Season getSeason() {
+    return season;
+  }
+
+  public void setSeason(Season season) {
+    this.season = season;
   }
 }
