1ce19f7e1a9a36a8e7a601f913dbec8eda68bfb6
Isaac Morris <xxjamesblond007xx@gmail.com>
2024-03-09 16:13:06 -0500

    Filled enum #51

--- Season.java
+++ Season.java
@@ -1,6 +1,26 @@
 public enum Season {
-  SPRING,
-  FALL,
-  WINTER,
-  SUMMER
+  SPRING("Spring", "SP", 1),
+  WINTER("Winter", "WI", 2),
+  FALL("Fall", "FA", 3),
+  SUMMER("Summer", "SU", 4);
+
+  String string;
+  String abbreviation;
+  int quarter;
+
+  Season(String string, String abbreviation, int quarter){
+    this.string = string;
+    this.abbreviation = abbreviation;
+    this.quarter = quarter;
+  }
+
+  public String toString(){
+    return string;
+  }
+  public String abbrevation(){
+    return abbreviation;
+  }
+  public int quarter(){
+    return quarter;
+  }
 }
