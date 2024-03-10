e88fe75e0f8a71954d3ee1ae7a98f30dd00e4b76
Isaac Morris <xxjamesblond007xx@gmail.com>
2024-03-10 19:32:20 -0400

    Added Status Methods #47

--- Status.java
+++ Status.java
@@ -2,16 +2,52 @@ import java.util.ArrayList;
 
 public enum Status {
     COMPLETE,
-    FINAL_STEP,
-    STARTED,
-    NOT_STARTED;
+    IN_PROGRESS,
+    NOT_STARTED,
+    UNSATISFACTORY;
 
-    public static Status getStatus(ArrayList<Credit> courses) {
-
+    public static Status getStatus(ArrayList<Credit> credits) {
+        boolean complete = false;
+        boolean inProgress = false;
+        boolean notStarted = false;
+        for(Credit credit : credits){
+            switch (Status.getStatus(credit)) {
+            case COMPLETE:
+                complete = true;
+                break;
+            case IN_PROGRESS:
+                inProgress = true;
+                break;
+            case NOT_STARTED: 
+                notStarted = true;
+            default:
+                break;
+            }
+        }
+        if(complete && !inProgress && !notStarted)
+            return COMPLETE;
+        if(notStarted && !complete && !inProgress)
+            return NOT_STARTED;
+        return IN_PROGRESS;
     }
     
-    public static Status getStatus(Credit course) {
+    public static Status getStatus(Credit credit) {
+        if(credit.getType() == CreditType.WITHDRAWN)
+            return UNSATISFACTORY;
         
+        int semesterComparison = credit.getSemesterTaken().compare(Semester.current());
+        switch (semesterComparison) {
+        case -1:
+            return NOT_STARTED;
+        case 0:
+            return IN_PROGRESS;
+        case 1:
+            break;
+        }
+
+        if(credit.getGrade() < 60)
+            return UNSATISFACTORY;
+        return COMPLETE;
     }
 }
 
