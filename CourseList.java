b47cc9b5c0c64e47d1a15ca2f0aed773c95bf145
JDCJDC103 <jdc015643@gmail.com>
2024-03-10 17:26:41 -0400

    made changes to issue #48

--- CourseList.java
+++ CourseList.java
@@ -14,36 +14,65 @@ public class CourseList {
     return courseList;
   }
 
-  public Course addCourse(String courseName, String courseID, String courseDescription, Int creditHours,
+  public Course addCourse(String courseName, String courseID, String courseDescription, int creditHours,
       ArrayList<Season> semesterAvailability, ArrayList<String> preRequisites, ArrayList<String> coRequisites,
       CourseType type) {
         boolean success = true;
         if (courseName == null
         || courseID == null
         || courseDescription == null
-        || creditHours == null
         || semesterAvailability == null
         || preRequisites == null
         || coRequisites == null
         || type == null) {
           return null;
   }
-        else return new Course(courseName, courseID, courseDescription, creditHours, semesterAvailability, preRequisites, coRequisites);
+        else {
+          Course newCourse = new Course(courseID, courseName, courseDescription, creditHours, semesterAvailability, preRequisites, coRequisites, type);
+          courses.add(newCourse);
+          return newCourse;
+        }
 }
 
   public boolean addCourse(Course course){
-
+    if (course != null) {
+      return courses.add(course);
+    }
+    return false;
   }
 
   public ArrayList<Course> addCourses(ArrayList<Course> courses){
-
+    boolean added = true;
+    for (Course course : courses) {
+      if (!courses.add(course)) {
+        added = false;
+      }
+    }
+    if (added) {
+      return courses;
+    }
+    return null;
   }
 
   public Course getCourse(String name) {
-    return courseName;
+    for (Course course : courses) {
+      if (course.getCourseName().equals(name)) {
+        return course;
+      }
+    }
+    return null;
   }
 
-  public Course getCourse(String id) {
-    return courseID;
+  public Course getCourseID(String id) {
+    for (Course course : courses) {
+      if (course.getCourseID().equals(id)) {
+        return course;
+      }
+    }
+    return null;
+  }
+
+  private void loadCourses() {
+    this.courses = DataLoader.getInstance().LoadCourses();
   }
 }
