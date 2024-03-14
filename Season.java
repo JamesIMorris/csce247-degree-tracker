97baaf46b00cdf566b4f56d41b79d323f4d3ca91
Isaac Morris <xxjamesblond007xx@gmail.com>
2024-03-14 03:44:09 -0400

    Added fromString #51

--- Season.java
+++ Season.java
@@ -24,4 +24,16 @@ public enum Season {
   public int quarter(){
     return quarter;
   }
+
+  static Season fromString(String string) {
+    if(string.toLowerCase().equals("spring"))
+      return SPRING;
+    if(string.toLowerCase().equals("summer"))
+      return SUMMER;
+    if(string.toLowerCase().equals("fall"))
+      return FALL;
+    if(string.toLowerCase().equals("winter"))
+      return WINTER;
+    return null;
+  }
 }
