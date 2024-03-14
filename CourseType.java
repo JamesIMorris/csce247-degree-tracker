1ff39ed8d316bae04c76f24d842ec56fef560ebd
Isaac Morris <xxjamesblond007xx@gmail.com>
2024-03-14 03:48:32 -0400

    Added fromString #50

--- CourseType.java
+++ CourseType.java
@@ -1,5 +1,15 @@
 public enum CourseType {
   DEFAUILT,
   OVERLAY,
-  PLACEMENT
+  PLACEMENT;
+
+  public static CourseType fromString(String string){
+    if(string.toLowerCase().equals("default"))
+      return DEFAUILT;
+    if(string.toLowerCase().equals("overlay"))
+      return OVERLAY;
+    if(string.toLowerCase().equals("placement"))
+      return PLACEMENT;
+    return null;
+  }
 }
