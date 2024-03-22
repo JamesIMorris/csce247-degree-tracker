b2691113ff6abbbdbc52137cffc5b70e7e9f0057
Isaac Morris <xxjamesblond007xx@gmail.com>
2024-03-22 16:28:20 -0400

    Fixed error #61

--- Category.java
+++ Category.java
@@ -1,37 +1,67 @@
 public enum Category {
-    REQUIREMENT(null, "Requirement"),
-    CC(REQUIREMENT, "Carolina Core"),
-    CR(REQUIREMENT, "College Requirement"),
-    PR(REQUIREMENT, "Program Requirement"),
-    MR(REQUIREMENT, "Major Requirement"),
-    FND(REQUIREMENT, "Founding Documents"),
-    CC_AIU(CC, "Asthetic and Interpretive Understanding"),
-    CC_ARP(CC, "Analytical Reasoning and Problem Solving"),
-    CC_CMS(CC, "Effective, Engaged, and Persuasive Communication: Spoken Component"),
-    CC_CMW(CC, "Effective, Engaged, and Persuasive Communication: Written"),
-    CC_GFL(CC, "Global Citizenship and Multicultural Understanding: Foreign Language"),
-    CC_GHS(CC, "Global Citizenship and Multicultural Understanding: Historival Thinking"),
-    CC_GSS(CC, "Global Citizenship and Multicultural Understanding: Social Sciences"),
-    CC_INF(CC, "Informative Literacy"),
-    CC_INT(CC, "Integrative Course"),
-    CC_SCI(CC, "Science Literacy"),
-    CC_VSR(CC, "Values, Ethics, and Social Responsibility"),
-    CC_IntegrativeCourse(CC, "Integrative Course"),
-    PR_FoundationalCourse(PR, "Foundational Course"),
-    PR_LabSci(PR, "Lab Science"),
-    PR_LibArts(PR, "Liberal Arts"),
-    PR_LowerDiv(PR, "Lower Division"),
-    PR_AppArea(PR, "Application Area"),
-    MR_MajorCourse(MR, "Major Course"),
-    MR_Elective(MR, "College Elective");
+    REQUIREMENT(null, "", "Requirement"),
+    CC(REQUIREMENT, "CC", "Carolina Core"),
+    CR(REQUIREMENT, "CR", "College Requirement"),
+    PR(REQUIREMENT, "PR", "Program Requirement"),
+    MR(REQUIREMENT, "MR", "Major Requirement"),
+    FND(REQUIREMENT, "FND", "Founding Documents"),
+    CC_AIU(CC, "CC-AIU", "Asthetic and Interpretive Understanding"),
+    CC_ARP(CC, "CC-ARP", "Analytical Reasoning and Problem Solving"),
+    CC_CMS(CC, "CC-CMS", "Effective, Engaged, and Persuasive Communication: Spoken Component"),
+    CC_CMW(CC, "CC-CMW", "Effective, Engaged, and Persuasive Communication: Written"),
+    CC_GFL(CC, "CC-GFL", "Global Citizenship and Multicultural Understanding: Foreign Language"),
+    CC_GHS(CC, "CC-GHS", "Global Citizenship and Multicultural Understanding: Historival Thinking"),
+    CC_GSS(CC, "CC-GSS", "Global Citizenship and Multicultural Understanding: Social Sciences"),
+    CC_INF(CC, "CC-INF", "Informative Literacy"),
+    CC_INT(CC, "CC-INT", "Integrative Course"),
+    CC_SCI(CC, "CC-SCI", "Science Literacy"),
+    CC_VSR(CC, "CC-VSR", "Values, Ethics, and Social Responsibility"),
+    CC_IntegrativeCourse(CC, "CC-IC", "Integrative Course"),
+    PR_FoundationalCourse(PR, "PR-FC", "Foundational Course"),
+    PR_LabSci(PR, "PR-LS", "Lab Science"),
+    PR_LibArts(PR, "PR-LA", "Liberal Arts"),
+    PR_LowerDiv(PR, "PR-LD", "Lower Division"),
+    PR_AppArea(PR, "PR-AA", "Application Area"),
+    MR_MajorCourse(MR, "MR-MC", "Major Course"),
+    MR_Elective(MR, "MR-E", "College Elective");
     
 
     Category parentCategory;
+    String abbreviation;
     String string;
 
-    Category(Category parentCategory, String string){
+    private void init(Category parentCategory, String abbreviation, String string){
         this.parentCategory = parentCategory;
+        this.abbreviation = abbreviation;
         this.string = string;
+    }
+
+    Category(Category parentCategory, String abbreviation, String string){
+        init(parentCategory, abbreviation, string);
+    }
+
+    Category(String abbreviation){
+        
+    }
+
+    public static Category fromAbbreviation(String abbreviation){
+        for(Category category : Category.values()){
+            if(category.getAbbreviation().equalsIgnoreCase(abbreviation))
+                return category;
+        }
+        return null;
+    }
+
+    public String getAbbreviation() {
+        return abbreviation;
+    }
+
+    public Category getParentCategory() {
+        return parentCategory;
+    }
+
+    public String getString() {
+        return string;
     }
 
     public Category parentCategory(){
