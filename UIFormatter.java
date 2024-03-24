5db6071f64c2cc77c897c6b402bc89d8d285afdf
Isaac Morris <xxjamesblond007xx@gmail.com>
2024-03-24 03:57:33 -0400

    Filled advisor methods and some student methods for UIFormatting #63

--- UIFormatter.java
+++ UIFormatter.java
@@ -1,6 +1,12 @@
 public class UIFormatter {
     public static String studentHomePage(String username){
-        
+        Student student = (Student)UserList.getInstance().findUser(username);
+        String homePage = "\n";
+        homePage += student.getFirstName() + " " + student.getLastName() + "\n";
+        homePage += "College: " + student.getMajor().getSchool();
+        homePage += "Major: " +student.getMajor().getName();
+        homePage += "Application Area: " + student.getApplicationArea();
+        //TODO
         return "";
     }
     public static String studentUnsatisfiedRequirements(String username){
@@ -12,15 +18,24 @@ public class UIFormatter {
         return "";
     }
     public static String adivsorHomePage(String username){
-
-        return "";
+        String homePage = "\n";
+        Advisor advisor = (Advisor)UserList.getInstance().findUser(username);
+        homePage += advisor.getFirstName() + " " +  advisor.getLastName() + "\n";
+        for(Student student : advisor.getAdvisees())
+            homePage += student.getFirstName() + " " + student.getLastName() + "(" + student.getUscID() + ")\n";
+        return homePage;
     }
     public static String advisorStudentPage(String studentUsername){
-
-        return "";
+        String studentHomepage = "\n";
+        studentHomepage += "Currently viewing " + studentUsername + "(" + ((Student)UserList.getInstance().findUser(studentUsername)).getUscID() + "):\n";
+        studentHomepage += studentHomePage(studentHomepage);
+        return studentHomepage;
     }
     public static String advisorNotes(String username){
-
-        return "";
+        String notes = "";
+        Student student = (Student)UserList.getInstance().findUser(username);
+        for(String note : student.getNotes())
+            notes += note + "\n";
+        return notes;
     }
 }
