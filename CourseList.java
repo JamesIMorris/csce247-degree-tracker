121bece329f56981fd03e616cf8983a17914950a
Isaac Morris <xxjamesblond007xx@gmail.com>
2024-03-24 22:07:10 -0400

    Adjusted methods #20

--- CourseList.java
+++ CourseList.java
@@ -43,19 +43,16 @@ public class CourseList {
   }
 
   public ArrayList<Course> addCourses(ArrayList<Course> courses) {
-    boolean added = true;
+    ArrayList<Course> addList = new ArrayList<Course>();
     for (Course course : courses) {
-      if (!courses.add(course)) {
-        added = false;
-      }
+      if (!this.courses.contains(course))
+        addList.add(course);
     }
-    if (added) {
-      return courses;
-    }
-    return null;
+    this.courses.addAll(addList);
+    return addList;
   }
 
-  public Course getCourse(String name) {
+  public Course getCourseFromName(String name) {
     for (Course course : courses) {
       if (course.getCourseName().equals(name)) {
         return course;
@@ -64,7 +61,7 @@ public class CourseList {
     return null;
   }
 
-  public Course getCourseID(String id) {
+  public Course getCourseFromID(String id) {
     for (Course course : courses) {
       if (course.getCourseID().equals(id)) {
         return course;
