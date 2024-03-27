766af99cd3c861b55f20b5bd47c7c73899be02a4
Isaac Morris <xxjamesblond007xx@gmail.com>
2024-03-26 21:36:03 -0400

    Fixed error #58

--- User.java
+++ User.java
@@ -14,7 +14,6 @@ public abstract class User{
     }
     public User(String username){
         this.username = username;
-        DataLoader.getInstance().loadUserData(this);
     }
 
     public String getUsername() {
